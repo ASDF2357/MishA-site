ed12552b992316fd9476667ca4e55c80179a1e79
ASDF2357 <67108097+ASDF2357@users.noreply.github.com>
2022-03-02 11:32:12 +0300

    step 2

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-from flask import Flask, escape
+from flask import Flask, escape, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    s = '''
-        <html>
-            <body>
-                <h1>Здесь HTML разметка</h1>
-                <p>Немного тектса тут и там</p>
-            </body>
-        </html>'''
-    return s
+# @app.route('/')
+# def hello_world():
+#     s = '''
+#         <html>
+#             <body>
+#                 <h1>Здесь HTML разметка</h1>
+#                 <p>Немного тектса тут и там</p>
+#             </body>
+#         </html>'''
+#     return s
+
+
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % ((escape(username)+" ")*10)
+
+
+@app.route('/main')
+def show_main():
+    return render_template("Main.html")
+
+
+@app.route('/about')
+def show_about():
+    return render_template("About.html")
+
+
+@app.route('/shop_list')
+def show_shop_list():
+    return render_template("ShopList.html")
 
 
 if __name__ == '__main__':
